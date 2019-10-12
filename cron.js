44c5fb09cdd06eca2cb487c969f85ca87b70924d
chrispiccaro <chrispiccaro18@gmail.com>
2019-10-12 10:49:09 -0700

    cron logic update

--- cron.js
+++ cron.js
@@ -1,12 +1,12 @@
 const schedule = require('node-schedule');
 const request = require('superagent');
 
-schedule.scheduleJob('0 0 * * *', () => {
-  console.log('The answer to life, the universe, and everything!');
+schedule.scheduleJob('0 0 * * *', async() => {
+  const newJobBoard = await getJobBoard();
 });
 
 const getJobBoard = async() => {
-  const res = await request.get('https://api.greenhouse.io/v1/boards/apptio/embed/departments');
+  const res = await request.get(process.env.JOB_BOARD);
   return res.body.departments
     .find(({ name }) => name === 'Engineering')
     .jobs.filter(({ location }) => location.name === 'Portland, OR');
