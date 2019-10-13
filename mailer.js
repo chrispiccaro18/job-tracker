7c20b93f40d65c0a3fdbb6b339f28728ec023a81
chrispiccaro <chrispiccaro18@gmail.com>
2019-10-13 10:48:13 -0700

    passing in some options to mailer

--- mailer.js
+++ mailer.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 // async..await is not allowed in global scope, must use a wrapper
-const mailer = async() => {
+const mailer = async(subject, text) => {
   // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
   // let testAccount = await nodemailer.createTestAccount();
@@ -18,11 +18,11 @@ const mailer = async() => {
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: 'chrispiccaro18@gmail.com', // sender address
-    to: 'laurenweiss830@gmail.com ', // list of receivers
-    subject: 'TESTING: I Love You', // Subject line
-    text: 'Hello cutie?', // plain text body
-    html: '<b>Hello cutie?</b>' // html body
+    from: process.env.EMAIL_ACCOUNT, // sender address
+    to: process.env.EMAIL_TO, // list of receivers
+    subject: subject, // Subject line
+    text: text, // plain text body
+    // html: '<b>Hello cutie?</b>' // html body
   });
 
   console.log('Message sent: %s', info.messageId);
